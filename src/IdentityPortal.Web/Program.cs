4113b9120b9429400ba7fa438a3f183c37fa8168
Jeremy Schlachter <jeremy.schlachter@west94.org>
2022-12-20 16:43:49 -0600

    Add view & controllers

--- src/IdentityPortal.Web/Program.cs
+++ src/IdentityPortal.Web/Program.cs
@@ -1,6 +1,80 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+#pragma warning disable SA1200 // UsingDirectivesMustBePlacedWithinNamespace
 
-app.MapGet("/", () => "Hello World!");
+using Serilog;
+using Serilog.Events;
 
-app.Run();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("logs/host-.log",
+        LogEventLevel.Debug,
+        rollingInterval: RollingInterval.Day,
+        fileSizeLimitBytes: 1_000_000,
+        rollOnFileSizeLimit: true,
+        shared: true,
+        flushToDiskInterval: TimeSpan.FromSeconds(1)
+    )
+    .CreateBootstrapLogger();
+
+Log.Information("Starting host");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    //
+    // Add services to the container.
+    builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+    builder.Host.UseSerilog((ctx, config) =>
+    {
+        config
+            .MinimumLevel.Is(ctx.HostingEnvironment.IsProduction() ? LogEventLevel.Warning : LogEventLevel.Debug)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .WriteTo.File("logs/application-.log",
+                rollingInterval: RollingInterval.Day,
+                fileSizeLimitBytes: 1_000_000,
+                rollOnFileSizeLimit: true,
+                shared: true,
+                flushToDiskInterval: TimeSpan.FromSeconds(1)
+            );
+
+        if (ctx.HostingEnvironment.IsDevelopment())
+        {
+            config.WriteTo.Console(
+                restrictedToMinimumLevel: LogEventLevel.Information);
+        }
+    });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.MapGet("/", () => "Hello World!");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unknown error occured on startup");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
